e5069e57e2d50d58583461fffec6b4ec1e10a011
firstquarterlee <firstquarterlee@gmail.com>
2024-06-06 23:56:05 +0900

    프로젝트 완성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(
+      Duration(seconds: 2),
+      (timer) {
+        int currentPage = controller.page!.toInt();
+        int nextPage = currentPage + 1;
+
+        if (nextPage > 4) {
+          nextPage = 0;
+        }
+
+        controller.animateToPage(
+            nextPage,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.linear, ///커브 애니메이션
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    if(timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: PageView(
+          controller: controller,
+          children: [1, 2, 3, 4, 5]
+              .map(
+                (e) => Image.asset(
+                  'asset/img/image_$e.jpeg',
+                  fit: BoxFit.cover,
+
+                  /// 이미지를 확대 시켜줌
+                ),
+              )
+              .toList(),
+        ),
+      ),
     );
   }
 }
